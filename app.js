7091c0294fe79f0759bb83a55594dfa883db99b9
Pragathi Venkatesh <pvenkatesh22@ucla.edu>
2019-04-07 03:46:59 -0700

    links updated

--- app.js
+++ app.js
@@ -1,27 +1,150 @@
-var http = require('http');
-var express = require('express');
-var ejs = require("ejs");
+const http = require('http');
+const express = require('express');
+const ejs = require('ejs');
+const fs = require('fs');
+
+var router = express.Router()
 var app = express();
+
+// read json data
+var dataPath = 'data/users.json'
+var data = fs.readFileSync(dataPath);
+var users = JSON.parse(data)
 
 app.set('view engine', 'ejs');
 
 // routes
 app.get('/', home);
-function home(req, res) {
-   res.render("header", {partial: "partials/home"});
-}
-
 app.get('/createUser', createUser);
-function createUser(req, res) {
-   res.render("header", {partial: "partials/createUser"});
-}
-
+app.get('/userCreated', userCreated);
+app.get('/login', login)
+app.get('/profile', profile)
 app.get('*', notFound)
-function notFound(res, res) {
-    res.send("error 404");
-}
+
 app.listen(3000, () => {console.log("server is running ...")});
 
 
+function home(req, res) {
+    res.render("header", {partial: "partials/home"});
+}
 
+function createUser(req, res) {
+    res.render("header", {partial: "partials/createUser"});
+    //, error: ""
+}
+
+function userCreated(req, res) {
+    console.log("usercreated function called")
+    if (doesUserExist(req.query.username)) {
+        router.get("/createUser");
+        console.log("error: " + req.query.username + " already created");
+        return;
+    }
+    console.log(req.query.username + " created");
+
+    var state = req.query.state;
+    var city = req.query.city;
+    var username = req.query.username;
+    var name = req.query.name
+
+    u = {};
+    u["state"] = state;
+    u["city"] = city;
+    u["name"] = name;
+
+    if (users[state] == undefined) {
+        users[state] = {};
+    }
+    if (users[state][city] == undefined) {
+        users[state][city] = {};
+    }
+    users[state][city][username] = u;
+    updateJson();
+    res.render("header", {partial: "partials/userCreated"});
+}
+
+function login(req, res) {
+    username = req.query.username;
+    if (!doesUserExist(username)) {
+        router.get('/login')
+        console.log("user " + username + " does not exist - cannot login")
+    }
+    res.render("header", {partial: "partials/login"})
+}
+
+function profile(req, res) {
+    var username = req.query.username;
+    var u = getUserObject(username);
+    console.log("u: " + JSON.stringify(u));
+    res.render("header", {partial: "partials/profile", uname: username, name: u.name, city: u.city, state: u.state, stores: u.fav})
+}
+
+function notFound(req, res) {
+    res.send("error 404");
+}
+
+
+// HELPER FUNCTIONS
+function doesUserExist(username) {
+    var state;
+    var city;
+
+    for(state in users){
+        for(city in users[state]){
+            if (users[state][city].hasOwnProperty(username)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function addUser(state, city, username, name, fav, needsRide){
+    if (doesUserExist(username)) {
+        return false;
+    }
+    data[state][city][username] = {
+        "name": name,
+        "fav": fav,
+        "needsRide": needsRide,
+        "city": city,
+        "state": state
+    }
+    return true;
+}
+
+function getBaes(user){
+    var matches = [];
+    var nMatches = 0;
+    var potMatch;
+    for (potMatch in data[user["state"]][user["city"]])
+    {
+        if(user["fav"] == potMatch["fav"])
+        {
+            matches[nMatches] = potMatch;
+            numOfMatches++;
+        }
+    }
+    return matches;
+}
+
+function updateJson() {
+    fs.writeFileSync(dataPath,JSON.stringify(users));
+}
+
+// returns null if user is not found
+function getUserObject(username) {
+    var state;
+    var city;
+
+    for(state in users){
+        for(city in users[state]){
+            if (users[state][city].hasOwnProperty(username)) {
+                console.log(users[state][city])
+                return users[state][city][username];
+            }
+        }
+    }
+    return null;
+}
 
