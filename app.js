b338647f04735fed4bf0c73b5b8e660bf87ba795
Pragathi Venkatesh <pvenkatesh22@ucla.edu>
2019-04-07 11:46:03 -0700

    added bobabae matching in profile

--- app.js
+++ app.js
@@ -21,9 +21,11 @@ app.get('/', home);
 app.get('/createUser', createUser);
 app.get('/userCreated', userCreated);
 app.get('/login', login)
+app.get('/editProfile', editUser) 
 app.get('/profile', profile)
 app.get('/schedule', scheduleBobaRun)
 app.get('/scheduleAdded', scheduleAdded)
+app.get('/about', about)
 app.get('*', notFound)
 
 app.listen(3000, () => {console.log("server is running ...")});
@@ -58,6 +60,10 @@ function userCreated(req, res) {
     u["city"] = city;
     u["name"] = name;
     u["fav"] = stores;
+    u["plans"] = [];
+    u["potential_matches"] = [];
+    u["requested_matches"] = [];
+    u["confirmed_matches"] = [];
 
     if (users[state] == undefined) {
         users[state] = {};
@@ -71,21 +77,27 @@ function userCreated(req, res) {
 }
 
 function login(req, res) {
-    username = req.query.username;
-    if (!doesUserExist(username)) {
-        router.get('/login')
-        console.log("user " + username + " does not exist - cannot login")
-    }
+    // username = req.query.username;
+    // if (!doesUserExist(username)) {
+    //     router.get('/login')
+    //     console.log("user " + username + " does not exist - cannot login")
+    // }
     res.render("header", {partial: "partials/login"})
 }
 
 function profile(req, res) {
     var username = req.query.username;
-    var u = getUserObject(username);
-    console.log("stores: " + JSON.stringify(u.fav));
-    res.render("header", {partial: "partials/profile", uname: username, name: u.name, city: u.city, state: u.state, stores: u.fav})
+    getBaes(username);
+    if (!doesUserExist(username)) {
+        router.get('/login')
+        console.log("user " + username + " does not exist - cannot login")
+    } else {
+        var u = getUserObject(username);
+        console.log("stores: " + JSON.stringify(u.fav));
+        res.render("header", {partial: "partials/profile", uname: username, name: u.name, city: u.city, state: u.state, 
+        stores: u.fav, potential_matches: u.potential_matches, requested_matches: u.requested_matches, confirmed_matches: u.confirmed_matches})
+    }
 }
-
 
 function scheduleBobaRun(req, res) {
     var username = req.query.username;
@@ -95,21 +107,55 @@ function scheduleBobaRun(req, res) {
 
 function scheduleAdded(req, res) {
     var username = req.query.username;
+    getBaes(username);
     var u = getUserObject(username);
     console.log('addschedule to ' + username);
     if (!u.hasOwnProperty('plans')) {
-        u.plans = []
+        u.plans = [];
+        u.plans.push(formatTime(req.query.earliestTime));
+        u.plans.push(formatTime(req.query.latestTime));
+    } else {
+        u.plans[0] = formatTime(req.query.earliestTime);
+        u.plans[1] = formatTime(req.query.latestTime);
     }
-    u['plans'].push([req.earliestTime, req.latestTime]);
-    res.render("header", {partial: "partials/scheduleAdded", uname: username, earliestTime: req.query.earliestTime, latestTime: req.query.latestTime});
+    updateJson();
+    res.render("header", {partial: "partials/profile", uname: username, name: u.name, city: u.city, state: u.state, 
+                            stores: u.fav, potential_matches: u.potential_matches, requested_matches: u.requested_matches, confirmed_matches: u.confirmed_matches})
+}
+
+function editUser(req, res) {
+    var username = req.query.username;
+    getBaes(username);
+    var requested
+    if (typeof(req.query.requested) === 'string') {
+        requested = [req.query.requested];
+    }
+    else { 
+        requested = req.query.requested;
+    }
+    var u = getUserObject(username);
+    for (var u2 in requested) {
+        console.log("u2: " + u2);
+        users[u.state][u.city][requested[u2]]["requested_matches"].push(username);
+    }
+    console.log(requested);
+    res.render("header", {partial: "partials/profile", uname: username, name: u.name, city: u.city, state: u.state, 
+                            stores: u.fav, potential_matches: u.potential_matches, requested_matches: u.requested_matches, confirmed_matches: u.confirmed_matches})
+    updateJson();
 }
 
 function notFound(req, res) {
     res.send("error 404");
 }
 
-
+function about(req, res) {
+    res.render("header", {partial: "partials/about"});
+}
 // HELPER FUNCTIONS
+function formatTime(timeString) {
+    return parseInt(timeString.substring(0,2) + timeString.substring(3,5));
+}
+
 function doesUserExist(username) {
     var state;
     var city;
@@ -138,19 +184,34 @@ function addUser(state, city, username, name, fav, needsRide){
     return true;
 }
 
-function getBaes(user){
-    var matches = [];
-    var nMatches = 0;
-    var potMatch;
-    for (potMatch in data[user["state"]][user["city"]])
-    {
-        if(user["fav"] == potMatch["fav"])
-        {
-            matches[nMatches] = potMatch;
-            numOfMatches++;
+function getBaes(username){
+    // var users
+    // var u = getUserObject(username);
+    // if (u.plans.length < 2) {
+    //     return;
+    // }
+    // for (var person in users[u.state]["Los Angeles"]) {
+    //   console.log(person)
+    //     if (users[u.state][u.city][person]["plans"].length == 2) {
+    //         var a = u.plans[0], b = u.plans[1], c = users[u.state][u.city][person]["plans"][0], d = users[u.state][u.city][person]["plans"][1];
+    //         if (!(c>b || d < a)) {
+    //           users[u.state][u.city][username]["potential_matches"].push(person);
+    //           break;
+    //         }
+    //     }
+    // }
+    // console.log(users[u.state][u.city][username]["potential_matches"])
+
+    var u = getUserObject(username);
+    var pm = []
+    for (var u2 in users[u.state][u.city]) {
+        if (u2 != username) {
+            pm.push(u2);
         }
     }
-    return matches;
+    console.log(pm);
+    users[u.state][u.city][username]["potential_matches"] = pm;
+    updateJson();
 }
 
 function updateJson() {
@@ -165,7 +226,6 @@ function getUserObject(username) {
     for(state in users){
         for(city in users[state]){
             if (users[state][city].hasOwnProperty(username)) {
-                console.log(users[state][city])
                 return users[state][city][username];
             }
         }
@@ -173,3 +233,12 @@ function getUserObject(username) {
     return null;
 }
 
+// assumes a & b are allready 
+function aRequestsb(usera, userb){
+    userb[requested_matches]+= "usera";
+}
+
+function bClicksOna(userb, usera){
+    usera[confirmed_matches]+= "userb";
+    userb[confirmed_matches]+= "usera";
+}
